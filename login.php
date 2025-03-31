82386c0fe3fd31e75ae3535104a5a1bd787b2bd8
hannahgraves3 <129108356+hannahgraves3@users.noreply.github.com>
2025-03-31 12:10:00 -0400

    Update login.php
    an update made after sevral days of in class work

--- login.php
+++ login.php
@@ -93,5 +93,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"></script>
 </body>
-</html>
-
+</html
